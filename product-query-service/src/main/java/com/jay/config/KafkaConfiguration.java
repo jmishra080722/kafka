a8c66914f1fd942d43c95fbb9d8149b940554d39
Jayhind Mishra <jmishra080722@gmail.com>
2025-01-10 00:29:35 +0530

    cqrs: product-quesry-service code

--- product-query-service/src/main/java/com/jay/config/KafkaConfiguration.java
+++ product-query-service/src/main/java/com/jay/config/KafkaConfiguration.java
@@ -1,2 +1,28 @@
-package com.jay.config;public class KafkaConfiguration {
+/*
+package com.jay.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.jay.dto");
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
 }
+*/
--- product-query-service/src/main/java/com/jay/service/ProductQueryService.java
+++ product-query-service/src/main/java/com/jay/service/ProductQueryService.java
@@ -1,2 +1,39 @@
-package com.jay.service;public class ProductQueryService {
+package com.jay.service;
+
+import com.jay.dto.ProductEvent;
+import com.jay.entity.Product;
+import com.jay.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import javax.sound.sampled.Port;
+import java.util.List;
+
+@Service
+public class ProductQueryService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> getProducts(){
+        return repository.findAll();
+    }
+
+    @KafkaListener(topics = "product-event-topic", groupId = "product-event-group")
+    public void processProductEvents(ProductEvent productEvent){
+        System.out.println("******************"+productEvent.getProduct().getName());
+        Product product = productEvent.getProduct();
+        if(productEvent.getEventType().equals("CreateEvent")){
+            repository.save(product);
+        }
+
+        if(productEvent.getEventType().equals("UpdateEvent")){
+            Product existingProduct = repository.findById(product.getId()).get();
+            existingProduct.setName(product.getName());
+            existingProduct.setDescription(product.getDescription());
+            existingProduct.setPrice(product.getPrice());
+            repository.save(existingProduct);
+        }
+    }
 }
